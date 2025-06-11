af31962a2d36e3004df289b9f4e580dbcf1e3538
skhasan536 <hasan.shaikh@wybrid.com>
2025-06-11 21:13:17 +0530

    directory-revision

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -4,7 +4,7 @@ export const studentRegisterValidate = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(4).alphanum().required(),
+    password: Joi.string().min(8).required(),
   });
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).json({ message: "Bad Request", error });
