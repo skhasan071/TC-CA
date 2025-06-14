bd1e83d26562e6533004624898e8624cd2d87a93
skhasan536 <hasan.shaikh@wybrid.com>
2025-06-14 17:22:37 +0530

    bad request fix

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -27,7 +27,7 @@ export const collegeRegisterValidate = (req, res, next) => {
   const schema = Joi.object({
     collegename: Joi.string().min(3).max(100).required(),
     collegeemail: Joi.string().email().required(),
-    collegepassword: Joi.string().min(4).alphanum().required(),
+    collegepassword: Joi.string().min(4).required(),
   });
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).json({ message: "Bad Request", error });
@@ -38,7 +38,7 @@ export const collegeRegisterValidate = (req, res, next) => {
 export const collegeLoginValidate = (req, res, next) => {
   const schema = Joi.object({
     collegeemail: Joi.string().email().required(),
-    collegepassword: Joi.string().min(4).alphanum().required(),
+    collegepassword: Joi.string().min(4).required(),
   });
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).json({ message: "Bad Request", error });
